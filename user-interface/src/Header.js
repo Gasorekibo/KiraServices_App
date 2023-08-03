a07625cf6e5c96d87250cb1beed4c40c4b6b26e1
JacquelineTuyisenge <j.tuyisenge@alustudent.com>
2023-08-03 21:55:43 +0200

    kira

--- user-interface/src/Header.js
+++ user-interface/src/Header.js
@@ -1,15 +1,21 @@
 // this contains navigation links and logout buttons
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; // Update this import
 
 const Header = () => {
-  const history = useHistory();
+  const navigate = useNavigate(); // Change useHistory to useNavigate
 
   const handleLogout = () => {
     // Implement logout functionality here, e.g., clearing user authentication state
-    // and redirecting to the login page//
-    localStorage.removeItem('userToken');
-    history.push('/user/login');
+    // and redirecting to the login page
+    // You can use local storage or a state management library (e.g., Redux) to manage user authentication state.
+    // For demonstration purposes, let's clear the authentication state using localStorage.
+
+    // Example: Clear the authentication state from local storage
+    localStorage.removeItem('userAuthToken');
+
+    // Navigate to the login page after logout
+    navigate('/user/login'); // Use navigate instead of history.push
   };
 
   return (
