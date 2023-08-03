4fe5a5d84d63b256b464e0ad064b456ad9d259d5
JacquelineTuyisenge <j.tuyisenge@alustudent.com>
2023-08-03 22:13:48 +0200

    kira

--- user-interface/src/App.js
+++ user-interface/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import SignUp from './SignUp';
 import Login from './Login';
 import Home from './Home';
@@ -8,19 +9,28 @@ import HospitalLogin from './HospitalLogin';
 import Header from './Header'; // Import the Header component
 
 function App() {
+  // Check if the user is logged in based on local storage
+  const userLoggedIn = localStorage.getItem('userLoggedIn');
+
   return (
     <Router>
-      <Header /> {/* Add the Header component */}
+      <Header /> {/* Render the Header component */}
       <Routes>
         <Route path="/user/signup" element={<SignUp />} />
         <Route path="/user/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/hospital/signup" element={<HospitalSignUp />} />
-        <Route path="/hospital/login" element={<HospitalLogin />} />
+        {userLoggedIn ? (
+          <>
+            <Route path="/home" element={<Home />} />
+            <Route path="/hospital/signup" element={<HospitalSignUp />} />
+            <Route path="/hospital/login" element={<HospitalLogin />} />
+          </>
+        ) : (
+          // Redirect to login if the user is not logged in
+          <Route path="*" element={<Navigate to="/user/login" />} />
+        )}
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
