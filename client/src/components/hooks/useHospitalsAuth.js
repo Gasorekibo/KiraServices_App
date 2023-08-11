f386b08a91c370c52104808d7b8f1141ac355cdc
Gasoremugwaneza <mugwanezagasore073@gmail.com>
2023-08-11 19:56:17 +0200

    Kira

--- client/src/components/hooks/useHospitalsAuth.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { HospitalAuthContext } from "../context/HospitalAuthProvider";
-
-const useHospitalAuth = () => {
-  return useContext(HospitalAuthContext);
-};
-
-export { useHospitalAuth };
--- server/routes/service.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createService } from "../controllers/service.js";
-
-const router = express.Router();
-
-router.post("/", createService);
-
-export default router;
