23c6f56197a664a2a512ebfcdb376b3a72d88ce8
Gasoremugwaneza <mugwanezagasore073@gmail.com>
2023-08-16 20:21:50 +0200

    hospital context

---
+++ data/hospitalContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useReducer } from "react";
+
+const userContext = createContext();
+const initialState = {
+  user: null,
+  loading: false,
+  error: false,
+};
+
+export const userReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        user: action.payload,
+      };
+    case "LOGOUT":
+      return {
+        user: null,
+      };
+  }
+};
+
+export const userContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(userReducer, initialState);
+
+  return (
+    <userContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </userContext.Provider>
+  );
+};
+export { userContextProvider, userContext };
